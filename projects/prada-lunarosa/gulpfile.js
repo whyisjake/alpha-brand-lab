f04c38a0d292574d408adeacc2b3be1a71e07c59
Fran Devinney <fdevinney@gmail.com>
2017-09-06 16:49:14 -0400

    working on pathing issues with site

--- projects/prada-lunarosa/gulpfile.js
+++ projects/prada-lunarosa/gulpfile.js
@@ -57,11 +57,6 @@ gulp.task('clean:www', function() {
   return del.sync('www');
 });
 
-// Gulp Run Sequence example
-//gulp.task('task-name', function(callback) {
-//  runSequence('task-one', 'task-two', 'task-three', callback);
-//});
-
 
 
 gulp.task('watch', ['browserSync', 'sass'], function (){
@@ -87,83 +82,3 @@ gulp.task('build', function (callback) {
   )
 })
 
-
-
-
-
-
-/*var gulp       = require('gulp'),
-    uglify     = require('gulp-uglify'),
-    size       = require('gulp-size'),
-    rename     = require('gulp-rename'),
-    stylus     = require('gulp-stylus'),
-    shell      = require('gulp-shell'),
-    watch      = require('gulp-watch'),
-    babel      = require('babelify'),
-    biffy      = require('browserify'),
-    nib        = require('nib'),
-    buffer     = require('vinyl-buffer'),
-    source     = require('vinyl-source-stream');
-
-gulp.task('default', ['stylus', 'stylint', 'js', 'eslint']);
-
-// Stylus
-gulp.task('stylus', function () {
-  return gulp.src('./public/css/_main.styl')
-    .pipe(stylus({
-        compress: true,
-        use: nib()
-    }))
-    .pipe( size({
-            title: 'Size of CSS:'
-    }) )
-    .pipe( rename({
-        basename: 'style',
-        extname: ".css"
-    }))
-    .pipe(gulp.dest('./public/css'));
-});
-
-// Stylint
-gulp.task('stylint', shell.task([
-    'stylint ./public/css/*.styl -c .stylintrc'
-]));
-
-// JS
-gulp.task( 'js', function () {
-    var b = biffy({
-        debug: false
-    });
-
-    b.transform( babel );
-
-    b.add( './public/js/_app.js' );
-
-    return b.bundle()
-        .on( "error", function (err) { console.log("Error: " + err.message); } )
-        .pipe( source('_app.js') )
-        .pipe( buffer() )
-        .pipe( uglify() )
-        .pipe( size({
-            title: 'Size of Javascript:'
-        }) )
-        .pipe( rename({
-            basename: 'app.min',
-            extname: ".js"
-        }))
-        .pipe( gulp.dest('./public/js/dist/') );
-});
-
-// ESLint
-gulp.task( 'eslint', shell.task([
-    'eslint ./public/js/_app.js'
-], { ignoreErrors: true }));
-
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch( ['./public/css/*.styl'], [ 'stylus', 'stylint' ] );
-    gulp.watch( ['./public/js/_app.js'], [ 'eslint', 'js' ] );
-});
-
-
-*/
