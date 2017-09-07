9a4c444e4def49cd8efd1f8fe0e97efa063934a2
Fran Devinney <fdevinney@gmail.com>
2017-09-07 10:58:23 -0400

    working on luna rosa build

--- projects/prada-lunarosa/gulpfile.js
+++ projects/prada-lunarosa/gulpfile.js
@@ -1,17 +1,9 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create(),
-    useref = require('gulp-useref'),
-    uglify = require('gulp-uglify'),
-    gulpIf = require('gulp-if'),
-    imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache'),
-    del = require('del'),
-    runSequence = require('run-sequence');
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
 
 
 
-gulp.task('sass', function(){
+/*gulp.task('sass', function(){
   return gulp.src('public/css/main.scss')
     .pipe(sass()
         .on('error', sass.logError)
@@ -20,17 +12,29 @@ gulp.task('sass', function(){
     .pipe(browserSync.reload({
       stream: true
     }))
+});*/
+
+gulp.task('webserver', function() {
+  gulp.src('public')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
 });
 
+/*
 gulp.task('browserSync', function() {
   browserSync.init({
     //server: {
     //  baseDir: 'public'
     //},
-    server: ["public", "www"],
+    server: ["public"],
   })
 })
+*/
 
+/*
 gulp.task('useref', function(){
   return gulp.src('public/*.html')
     .pipe(useref())
@@ -38,15 +42,18 @@ gulp.task('useref', function(){
     .pipe(gulpIf('*.js', uglify()))
     .pipe(gulp.dest('www'))
 });
+*/
 
-gulp.task('images', function(){
-  return gulp.src('public/img/**/*.+(png|jpg|gif|svg)')
-  .pipe(cache(imagemin({
-      interlaced: true
-    })))
-  .pipe(gulp.dest('www/img'))
-});
 
+//gulp.task('images', function(){
+  //return gulp.src('public/img/**/*.+(png|jpg|gif|svg)')
+  //.pipe(cache(imagemin({
+   //   interlaced: true
+  //  })))
+  //.pipe(gulp.dest('www/img'))
+//});
+
+/*
 // Copies the index.html page into 'www' folder during build task
 gulp.task('indexpage', function() {
   return gulp.src('public/index.html')
@@ -56,14 +63,14 @@ gulp.task('indexpage', function() {
 gulp.task('clean:www', function() {
   return del.sync('www');
 });
+*/
 
 
 
-gulp.task('watch', ['browserSync', 'sass'], function (){
+/*gulp.task('watch', ['browserSync', 'sass'], function (){
     gulp.watch( ['public/css/*.scss'], [ 'sass'] );
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('public/*.html', browserSync.reload); 
-    //gulp.watch( ['./public/js/_app.js'], [ 'eslint', 'js' ] );
 });
 
 
@@ -80,5 +87,4 @@ gulp.task('build', function (callback) {
     ['sass', 'useref', 'images', 'indexpage'],
     callback
   )
-})
-
+})*/
