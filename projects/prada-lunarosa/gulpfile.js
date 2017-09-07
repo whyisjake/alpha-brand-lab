bd3bcead3265a4f0562f747c9666421a29d67289
Fran Devinney <fdevinney@gmail.com>
2017-09-07 12:01:42 -0400

    getting slimmer gulpfile working

--- projects/prada-lunarosa/gulpfile.js
+++ projects/prada-lunarosa/gulpfile.js
@@ -1,90 +1,31 @@
 var gulp = require('gulp');
-var webserver = require('gulp-webserver');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 
-
-/*gulp.task('sass', function(){
-  return gulp.src('public/css/main.scss')
-    .pipe(sass()
-        .on('error', sass.logError)
-    )
-    .pipe(gulp.dest('www/css'))
+gulp.task('sass', function() {
+  return gulp.src('public/sass/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
-});*/
-
-gulp.task('webserver', function() {
-  gulp.src('public')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
 });
 
-/*
 gulp.task('browserSync', function() {
   browserSync.init({
-    //server: {
-    //  baseDir: 'public'
-    //},
-    server: ["public"],
+    server: {
+      baseDir: 'public'
+    },
   })
 })
-*/
-
-/*
-gulp.task('useref', function(){
-  return gulp.src('public/*.html')
-    .pipe(useref())
-    // Minifies only if it's a JavaScript file
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulp.dest('www'))
-});
-*/
 
 
-//gulp.task('images', function(){
-  //return gulp.src('public/img/**/*.+(png|jpg|gif|svg)')
-  //.pipe(cache(imagemin({
-   //   interlaced: true
-  //  })))
-  //.pipe(gulp.dest('www/img'))
-//});
-
-/*
-// Copies the index.html page into 'www' folder during build task
-gulp.task('indexpage', function() {
-  return gulp.src('public/index.html')
-  .pipe(gulp.dest('www'))
-});
-
-gulp.task('clean:www', function() {
-  return del.sync('www');
-});
-*/
-
-
-
-/*gulp.task('watch', ['browserSync', 'sass'], function (){
-    gulp.watch( ['public/css/*.scss'], [ 'sass'] );
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('public/*.html', browserSync.reload); 
-});
-
-
-gulp.task('default', function (callback) {
-  runSequence(['sass','browserSync', 'watch'],
-    callback
-  )
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('public/sass/**/*.scss', ['sass']); 
+  // Other watchers
+  gulp.watch('public/*.html', browserSync.reload); 
+  gulp.watch('public/js/**/*.js', browserSync.reload); 
 })
 
 
-// Build Files
-gulp.task('build', function (callback) {
-    runSequence('clean:www', 
-    ['sass', 'useref', 'images', 'indexpage'],
-    callback
-  )
-})*/
